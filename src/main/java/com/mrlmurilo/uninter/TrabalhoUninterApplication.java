321c2319fe23f1acca3a1f53dfb0b8f59a6c47d8
Murilo <murilomargonar@gmail.com>
2025-12-29 13:40:59 -0300

    inital commit

---
+++ src/main/java/com/mrlmurilo/uninter/TrabalhoUninterApplication.java
@@ -0,0 +1,14 @@
+package com.mrlmurilo.uninter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoUninterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoUninterApplication.class, args);
+		System.out.println("Teste");
+	}
+
+}
---
+++ src/test/java/com/mrlmurilo/uninter/TrabalhoUninterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrlmurilo.uninter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoUninterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
