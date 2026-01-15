e0216b8da060c8b99b817c82bba9481d8bf37ab7
Murilo <murilomargonar@gmail.com>
2026-01-15 19:42:34 -0300

    Create ConsultaController.java

---
+++ src/main/java/com/mrlmurilo/uninter/controller/ConsultaController.java
@@ -0,0 +1,33 @@
+package com.mrlmurilo.uninter.controller;
+
+import com.mrlmurilo.uninter.domain.consulta.Consulta;
+import com.mrlmurilo.uninter.dto.consulta.ConsultaResponse;
+import com.mrlmurilo.uninter.dto.consulta.CriarConsultaRequest;
+import com.mrlmurilo.uninter.service.ConsultaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/consultas")
+@RequiredArgsConstructor
+public class ConsultaController {
+
+    private final ConsultaService consultaService;
+
+    @PostMapping
+    public ConsultaResponse criar(@RequestBody CriarConsultaRequest request) {
+        return consultaService.criar(request);
+    }
+
+    @GetMapping
+    public List<ConsultaResponse> listar() {
+        return consultaService.listar();
+    }
+
+    @GetMapping("/{id}")
+    public ConsultaResponse buscarPorId(@PathVariable Long id) {
+        return consultaService.buscarPorId(id);
+    }
+}
