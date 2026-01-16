28829c08bf1f8603e9a86d262d90ffb77cd5eb40
Murilo <murilomargonar@gmail.com>
2026-01-16 18:31:10 -0300

    Create AuthService.java

---
+++ src/main/java/com/mrlmurilo/uninter/service/AuthService.java
@@ -0,0 +1,34 @@
+package com.mrlmurilo.uninter.service;
+
+import com.mrlmurilo.uninter.dto.auth.LoginRequest;
+import com.mrlmurilo.uninter.dto.auth.LoginResponse;
+import com.mrlmurilo.uninter.security.jwt.JwtService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+    private final AuthenticationManager authManager;
+    private final JwtService jwtService;
+
+    public LoginResponse login(LoginRequest request) {
+
+        Authentication authentication = authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.email(),
+                        request.password()
+                )
+        );
+
+        UserDetails user = (UserDetails) authentication.getPrincipal();
+        String token = jwtService.gerarToken(user);
+
+        return new LoginResponse(token);
+    }
+}
