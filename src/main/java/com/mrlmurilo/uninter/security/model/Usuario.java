2b343fc9d55d8d3cd3cd151ec8cf5c28c9f6c116
Murilo <murilomargonar@gmail.com>
2026-01-16 18:31:14 -0300

    Create Usuario.java

---
+++ src/main/java/com/mrlmurilo/uninter/security/model/Usuario.java
@@ -0,0 +1,28 @@
+package com.mrlmurilo.uninter.security.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "usuarios")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String senha;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+}
